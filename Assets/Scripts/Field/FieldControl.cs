718ae8bab73de8f92b40c7844a6551c479747355
Ximo <xmalbors@hotmail.com>
2017-06-21 16:33:29 +0200

    FieldControl: SpawnThrower: Búsqueda del lanzador usando su "assetName"

--- Assets/Scripts/Field/FieldControl.cs
+++ Assets/Scripts/Field/FieldControl.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 public class FieldControl : MonoBehaviour {
 
@@ -161,7 +162,7 @@ public class FieldControl : MonoBehaviour {
   void SpawnThrower()
   {
     Transform spawn = GameObject.Find("throwerSpawn").transform;
-    GameObject.Instantiate( throwerPrefab[ThrowerObject.idModelo], spawn.position, spawn.rotation );
+    GameObject.Instantiate( throwerPrefab.First( prefab => prefab.name.Contains(ThrowerObject.assetName) ), spawn.position, spawn.rotation );
   }
 
   void Start() {
